b7094c88d465ede4e10aa89b1a68652d8672aba2
kaballl <76085266+kaballl@users.noreply.github.com>
2021-11-29 21:14:46 +0700

    sds

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 async function connect(){
     try{
-        await mongoose.connect('mongodb://localhost:/PTUDW',{
+        await mongoose.connect('mongodb://localhost:27017/PTUDW',{
             useNewUrlParser:true,
             useUnifiedTopology:true
         });
